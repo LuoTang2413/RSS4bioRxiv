afb5aab5a1be5147541d25c33b5bb9c3c50d23cf
LuoTang2413 <124507297+LuoTang2413@users.noreply.github.com>
2023-10-09 22:05:17 +0800

    Update bioRxiv-subscriber.js

--- bioRxiv-subscriber.js
+++ bioRxiv-subscriber.js
@@ -1,31 +1,36 @@
-// 定义订阅源的URL
-const rssFeedUrl = "http://connect.biorxiv.org/biorxiv_xml.php?subject=all";
+const express = require('express');
+const axios = require('axios');
+const xml2js = require('xml2js');
 
-// 获取订阅源的数据
-fetch(rssFeedUrl)
-  .then((response) => response.text())
-  .then((xmlData) => {
-    // 解析XML数据
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+const app = express();
+const port = 3000;
 
-    // 提取文章标题和链接
-    const items = xmlDoc.querySelectorAll("item");
-    const subscriptionList = document.getElementById("subscription-list");
+app.get('/biorxiv-updates', async (req, res) => {
+    try {
+        // Fetch data from the BioRxiv XML feed
+        const response = await axios.get('http://connect.biorxiv.org/biorxiv_xml.php?subject=all');
+        const xmlData = response.data;
 
-    items.forEach((item) => {
-      const title = item.querySelector("title").textContent;
-      const link = item.querySelector("link").textContent;
+        // Parse XML data to JSON
+        const parser = new xml2js.Parser();
+        parser.parseString(xmlData, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: 'Error parsing XML data' });
+            } else {
+                const entries = result.feed.entry;
+                const updates = entries.map(entry => ({
+                    title: entry.title[0],
+                    link: entry.link[0].$.href,
+                    summary: entry.summary[0]._
+                }));
+                res.json(updates);
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from BioRxiv' });
+    }
+});
 
-      // 创建列表项并添加到页面中
-      const listItem = document.createElement("li");
-      const linkElement = document.createElement("a");
-      linkElement.href = link;
-      linkElement.textContent = title;
-      listItem.appendChild(linkElement);
-      subscriptionList.appendChild(listItem);
-    });
-  })
-  .catch((error) => {
-    console.error("获取订阅数据时出错：", error);
-  });
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
