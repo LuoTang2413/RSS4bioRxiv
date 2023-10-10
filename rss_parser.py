f78ddf3ed79d8eadfbf82a44a50b5adbfaa594e1
LuoTang2413 <124507297+LuoTang2413@users.noreply.github.com>
2023-10-10 20:36:57 +0800

    Update rss_parser.py

--- rss_parser.py
+++ rss_parser.py
@@ -1,4 +1,5 @@
 import feedparser
+from jinja2 import Environment, FileSystemLoader
 
 # 解析新的RSS源
 new_rss_url = 'http://new-rss-source.com/feed.xml'
@@ -13,19 +14,19 @@ if len(feed.entries) > 0:
     abstract = entry.abstract
     pub_date = entry.published
 
-    # 读取现有的index.html内容
-    with open('index.html', 'r') as file:
-        html_content = file.read()
-
-    # 替换HTML内容中的相应标记，将新的数据插入
-    html_content = html_content.replace('{{Title}}', title)
-    html_content = html_content.replace('{{Author}}', author)
-    html_content = html_content.replace('{{DOI}}', doi)
-    html_content = html_content.replace('{{Abstract}}', abstract)
-    html_content = html_content.replace('{{PubDate}}', pub_date)
+    # 使用Jinja2模板引擎来动态生成HTML内容
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('template.html')
+    rendered_html = template.render(
+        Title=title,
+        Author=author,
+        DOI=doi,
+        Abstract=abstract,
+        PubDate=pub_date
+    )
 
     # 保存更新后的index.html
     with open('index.html', 'w') as file:
-        file.write(html_content)
+        file.write(rendered_html)
 else:
     print('没有找到新的RSS数据。')
