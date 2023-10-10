3b1c1c31f473e885c1f825a550b78e7ed91e7aab
LuoTang2413 <124507297+LuoTang2413@users.noreply.github.com>
2023-10-10 20:50:17 +0800

    Update rss_parser.py

--- rss_parser.py
+++ rss_parser.py
@@ -1,32 +1,38 @@
 import feedparser
-from jinja2 import Environment, FileSystemLoader
 
-# 解析新的RSS源
-new_rss_url = 'http://new-rss-source.com/feed.xml'
-feed = feedparser.parse(new_rss_url)
+# 解析RSS源
+rss_url = "http://connect.biorxiv.org/biorxiv_xml.php?subject=all"
+feed = feedparser.parse(rss_url)
 
-# 获取需要的数据（示例中仅获取第一条数据）
-if len(feed.entries) > 0:
-    entry = feed.entries[0]
+# 创建一个列表，用于存储RSS条目
+rss_entries = []
+
+# 遍历RSS数据并将它们添加到列表中
+for entry in feed.entries:
     title = entry.title
     author = entry.author
     doi = entry.doi
     abstract = entry.abstract
     pub_date = entry.published
 
-    # 使用Jinja2模板引擎来动态生成HTML内容
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template('template.html')
-    rendered_html = template.render(
-        Title=title,
-        Author=author,
-        DOI=doi,
-        Abstract=abstract,
-        PubDate=pub_date
-    )
+    rss_entries.append({
+        "title": title,
+        "author": author,
+        "doi": doi,
+        "abstract": abstract,
+        "pub_date": pub_date
+    })
 
-    # 保存更新后的index.html
-    with open('index.html', 'w') as file:
-        file.write(rendered_html)
-else:
-    print('没有找到新的RSS数据。')
+# 以HTML格式生成RSS内容
+html_content = "<html><head><title>RSS Feed</title></head><body>"
+for entry in rss_entries:
+    html_content += f"<h2>{entry['title']}</h2>"
+    html_content += f"<p>Author: {entry['author']}</p>"
+    html_content += f"<p>DOI: {entry['doi']}</p>"
+    html_content += f"<p>Abstract: {entry['abstract']}</p>"
+    html_content += f"<p>Published Date: {entry['pub_date']}</p>"
+html_content += "</body></html>"
+
+# 保存HTML内容到index.html
+with open('index.html', 'w', encoding='utf-8') as file:
+    file.write(html_content)
